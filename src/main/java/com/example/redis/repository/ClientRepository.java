f270ca6427d9ba3066b86fde19cda6fa9169c10d
Mauro-Pereira <mauropereira@gmail.com>
2024-07-26 14:31:41 -0300

    feat: add service

--- src/main/java/com/example/redis/repository/ClientRepository.java
+++ src/main/java/com/example/redis/repository/ClientRepository.java
@@ -1,5 +1,7 @@
 package com.example.redis.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.redis.model.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
-    
+    Optional<Client> findByEmail(String email);
 }
--- src/main/java/com/example/redis/service/ClientService.java
+++ src/main/java/com/example/redis/service/ClientService.java
@@ -1,6 +1,12 @@
 package com.example.redis.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.redis.model.Client;
@@ -10,10 +16,56 @@ import com.example.redis.repository.ClientRepository;
 public class ClientService {
 
     @Autowired
-    private ClientRepository clienRepository;
+    private ClientRepository clientRepository;
 
+    @Cacheable(value = "Client", key = "#id")
     public Client createClient(Client client){
+
+        Optional<Client> returnedClient = this.clientRepository.findByEmail(client.getEmail());
         
+        if(returnedClient.isEmpty()){
+            System.out.println("I must to return a exception here after");
+        }
+
+        return this.clientRepository.save(client);
     }
+
+    @Cacheable(value = "Client")
+    public List<Client> returnAllClient(){
+        return this.clientRepository.findAll();
+    }
+
+    @CachePut(value = "Client", key = "#id")
+    public Client updateClient(Long id, Client client){
+
+        Optional<Client> returnedClient = this.clientRepository.findById(id);
+
+        if(returnedClient.isEmpty()){
+            System.out.println("I must to return a exception here after");
+        }
+
+        Client updatedClient = returnedClient.get(); 
+        
+        updatedClient.setName(client.getName());
+        updatedClient.setSurname(client.getSurname());
+        updatedClient.setEmail(client.getEmail());
+        updatedClient.setAdmin(client.isAdmin());
+        updatedClient.setPassword(client.getPassword());
+
+        return updatedClient;
+    }
+
+    @CacheEvict(value = "Client", key = "#id")
+    public void removeClient(Long id){
+        Optional<Client> returnedClient = this.clientRepository.findById(id);
+
+        if(returnedClient.isEmpty()){
+            System.out.println("I must to return a exception here after");
+        }
+
+        this.clientRepository.deleteById(id);
+    }
+
+
 
 }
